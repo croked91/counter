5bcb0864e531fe17c344182fba255d6f60741250
Croked1991 <atik.egor@gmail.com>
2022-05-25 09:47:11 +0300

    fixed some bugs with localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,58 @@
 import './App.css';
-import React, {useState} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { stat } from 'fs';
 
 type IncrementerType = {
   state: number
-  inc: ()=>void
+  inc: () => void
 }
 
-const Incrementer = (props:IncrementerType) => {
-  if (props.state !== 5) 
-    {return(<button onClick={props.inc}>inc</button>)}
+const Incrementer = (props: IncrementerType) => {
+  if (props.state !== 5) { return (<button onClick={props.inc}>inc</button>) }
   return (<button disabled onClick={props.inc}>inc</button>)
 
 
 }
 
+
+
 function App() {
-  let [state, setState] = useState(0)
   
+  const [state, setState] = useState<number>(JSON.parse(localStorage.getItem("counterValue") || "0"))
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("counterValue") || "0")
+    setState(data)   
+}, [])
+
+  useEffect(() => {
+    localStorage.setItem("counterValue", JSON.stringify(state))
+  }, [state])
+ 
+
+  const getter = () => {
+    let valueAsAString = localStorage.getItem("counterValue")
+    if (valueAsAString) {
+      let newValue = JSON.parse(valueAsAString)
+      setState(newValue)
+  }}
+
+  
+
   const inc = () => {
-   if (state <= 4)  setState(state+1)
+    if (state <= 4) setState((prev)=>prev+1)
   }
 
   const reset = () => {
     setState(0)
-   }
-  
+  }
+
   return (
     <div >
       {state}
-      <Incrementer inc={inc} state={state}/>
+      <Incrementer inc={inc} state={state} />
       <button onClick={reset}>reset</button>
+      <button onClick={getter}>getter</button>
     </div>
   );
 }
